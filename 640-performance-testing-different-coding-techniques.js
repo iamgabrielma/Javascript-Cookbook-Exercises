24ea88aa77f59d1bf99800335ede4f0b847e2ce5
gma992 <gabrielmaldonado.art@gmail.com>
2016-03-01 23:47:14 +0900

    nothing to solve here, just introduction to jsPerf, selenium and profiling

---
+++ 640-performance-testing-different-coding-techniques.js
@@ -0,0 +1,8 @@
+/*
+Problem
+In JavaScript there are, typically, multiple ways you can code a solution. The problem then becomes determining which of the different ways performs best across different environments.
+*/
+
+// approach 1: use performance testing tool like jsPerf or selenium for node.js
+
+// approach 2: use profiling, example built-in in firebug firefox under "profile" button, chrome profiles tab
